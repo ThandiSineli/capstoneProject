eb8fc2d029214fcc8083a136755122492c539a15
Thandeka Sineli <Thandekasineli@gmail.com>
2024-03-19 11:10:45 +0200

    checking

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { config } from 'dotenv';
-
 import productRoutes from './routes/productRoutes.js'; 
 import usersRoutes from './routes/usersRoutes.js';
-
 import cartRoutes from './routes/cartRoutes.js';
 
 config();
@@ -13,18 +11,16 @@ config();
 const app = express();
 
 app.use(cors());
-
-
 app.use(express.json());
 app.use(cookieParser());
 
+// Serve static files from the public directory
 
-
-
+app.use(express("/public"));
 
 // Use product routes (and other routes as needed)
 app.use('/', productRoutes);
-app.use('/',usersRoutes)
+app.use('/', usersRoutes);
 app.use('/', cartRoutes);
 
 // Handle 404 Not Found
